80acd30b84328708e5423dc9a8729e875bf4deba
AnnaS30 <windywind@bigmir.net>
2023-03-26 21:43:46 +0100

    added axios, API integration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <div>Weather App</div>
-          <button className="btn btn-primary">Hello</button>
-        </header>
+        <Weather />
+
+        <footer>
+          This project was coded by AnnaS and is open-sourced on{" "}
+          <a href="https://www.ukr.net/ua/">GitHub</a> and hosted on{" "}
+          <a href="https://www.ukr.net/ua/">Netlify</a>
+        </footer>
       </div>
     </div>
   );
